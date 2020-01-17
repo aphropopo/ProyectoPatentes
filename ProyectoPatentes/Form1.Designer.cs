dc3eb80b0ffc72ce658d1867110bc0940526c2da
aphropopo <francodx@gmail.com>
2020-01-17 00:17:55 -0300

    Creacion de lo visual.

--- ProyectoPatentes/Form1.Designer.cs
+++ ProyectoPatentes/Form1.Designer.cs
@@ -28,13 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPatente = new System.Windows.Forms.TextBox();
+            this.btnPatente = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAuto = new System.Windows.Forms.TextBox();
+            this.btnAuto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Script MT Bold", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(151, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 37);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Dameri";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cooper Black", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(377, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Presupuestos:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(264, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Script MT Bold", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(406, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 37);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Dameri";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(381, 132);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(247, 256);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 16);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Buscar Patente:";
+            // 
+            // txtPatente
+            // 
+            this.txtPatente.Location = new System.Drawing.Point(150, 195);
+            this.txtPatente.Name = "txtPatente";
+            this.txtPatente.Size = new System.Drawing.Size(108, 20);
+            this.txtPatente.TabIndex = 6;
+            // 
+            // btnPatente
+            // 
+            this.btnPatente.Location = new System.Drawing.Point(264, 189);
+            this.btnPatente.Name = "btnPatente";
+            this.btnPatente.Size = new System.Drawing.Size(75, 28);
+            this.btnPatente.TabIndex = 7;
+            this.btnPatente.Text = "Buscar";
+            this.btnPatente.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(34, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(95, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Buscar Auto:";
+            // 
+            // txtAuto
+            // 
+            this.txtAuto.Location = new System.Drawing.Point(150, 238);
+            this.txtAuto.Name = "txtAuto";
+            this.txtAuto.Size = new System.Drawing.Size(108, 20);
+            this.txtAuto.TabIndex = 6;
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(264, 232);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(75, 28);
+            this.btnAuto.TabIndex = 7;
+            this.btnAuto.Text = "Buscar";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(639, 397);
+            this.Controls.Add(this.btnAuto);
+            this.Controls.Add(this.btnPatente);
+            this.Controls.Add(this.txtAuto);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPatente);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Buscador de Presupuestos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPatente;
+        private System.Windows.Forms.Button btnPatente;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAuto;
+        private System.Windows.Forms.Button btnAuto;
     }
 }
 
--- ProyectoPatentes/Form1.cs
+++ ProyectoPatentes/Form1.cs
@@ -16,5 +16,25 @@ namespace ProyectoPatentes
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
